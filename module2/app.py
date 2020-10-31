ca71c4fe1000ab920df107379c7dc2f47c3acbc1
Amar Gowda <amgowda@microsoft.com>
2020-10-31 15:39:40 -0700

    changes for it to make it work

--- module2/app.py
+++ module2/app.py
@@ -9,18 +9,28 @@ import time
 from datetime import datetime
 
 app = Flask(__name__)
+
+
 api = Api(app)
 
 
 
 class Math(Resource):
     def get(self, runid):
-        return Response({"error": "unknown patient_id"}, status=404, mimetype='application/json')
+        return Response({"error": "unknown simulation"}, status=404, mimetype='application/json')
 
     def post(self, runid):
         body = request.get_json(silent=True)
+        #logging.ad
         print (str(body))
         retarray = np.random.randint(body["value"], size=1)
+    
+        #out = jsonify ({"retvalue": str(retarray[0]), "runid": runid, "executedonon":datetime.now()})
+        print(str(retarray[0]))
+       
+        #out = jsonify({"retvalue": retarray[0], "runid": runid, "executedonon":datetime.now()})
+        #print(str(out))
+        #return Response({"allgood here"}, status=200, mimetype='application/json')
         return jsonify ({"retvalue": str(retarray[0]), "runid": runid, "executedonon":datetime.now()  })
 
 
@@ -30,4 +40,9 @@ api.add_resource(Math, '/domath/<string:runid>')
 
 if __name__ == '__main__':
     app.debug = False
+
+    import logging
+
+    logging.basicConfig(filename='error.log',level=logging.DEBUG)
+
     app.run(host='0.0.0.0', port=4996, threaded=True)
